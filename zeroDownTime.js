c26a89927f57cb7cacebcc2bcd3b73968024b6ef
ambaqinejad <ambaqinejad@gmail.com>
2021-06-09 12:43:15 +0430

    zero down time

--- zeroDownTime.js
+++ zeroDownTime.js
@@ -7,6 +7,16 @@ if (cluster.isMaster) {
   for (let i = 0; i < numOfCpus; i++) {
     cluster.fork();
   }
+
+  cluster.on("exit", (worker) => {
+    console.log(`${worker.process.pid} has died`);
+    cluster.fork();
+    console.log('new worker has attached.');
+    console.log('Remaining');
+    for (const worker of Object.values(cluster.workers)) {
+        console.log(worker.process.pid);
+    }
+  });
 } else {
   console.log(`This is the worker process with pid: ${process.pid}`);
   http
@@ -18,6 +28,12 @@ if (cluster.isMaster) {
           processId: process.pid,
         })
       );
+
+      if (req.url === "/") {
+        console.log(`Serving from process ${process.pid}`);
+      } else if (req.url === '/kill') {
+        process.exit();
+      }
     })
     .listen(3000);
 }
